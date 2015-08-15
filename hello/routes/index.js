ab52f956c73170aada50815651fbb4c5d4762757
lingjuan.guan@qunar.com <lingjuanguan@guanlingjuandeMacBook-Pro.local>
2015-08-15 20:23:34 +0800

    add

--- hello/routes/index.js
+++ hello/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: '关令娟的开心聊天室' });
 });
 
 module.exports = router;
--- hello/routes/users.js
+++ hello/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '关令娟的开心聊天室' });
 });
 
 module.exports = router;
